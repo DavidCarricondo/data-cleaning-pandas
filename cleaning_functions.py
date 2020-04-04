0c1fa82dd30c9270feedfca08524a10eb63adff9
DavidCarricondo <dacsmail00@gmail.com>
2020-04-04 20:30:25 +0200

    Safety save

--- cleaning_functions.py
+++ cleaning_functions.py
@@ -3,6 +3,8 @@
 #dependencies:
 
 import re
+import countryinfo as cti #https://pypi.org/project/countryinfo/
+
 
 ##Column names to lower case removing blank spaces from beginning and end and subituttin them by _ in the middle,
 # it also removes comments between brackets after the name of the column:
@@ -49,7 +51,34 @@ def timeclassify(lst):
             else: l.append(None)
     return l
     
+#Function to categorize activity into 5 categories:
 
-##Function to fix activity
+def fixactiv(lst):
+    l = []
+    for e in lst:
+        if any(x in e.lower() for x in ['surf', 'boar','padd','kay']):
+            l.append('surfing')
+        elif any(x in e.lower() for x in ['spear', 'fish','hunt']):
+            l.append('fishing')
+        elif any(x in e.lower() for x in ['swim', 'snork', 'scub','div','bath','float','jump','play']):
+            l.append('swimming')
+        elif any(x in e.lower() for x in ['wading', 'stand', 'walk', 'sit','wash','splash']):
+            l.append('standing')
+        else: l.append('other')
+    return l
 
-#Function to fix life
+##Function to extract country hemisphere. This function just uses countryinfo module 
+#  to check whether the latitude of a country is positive or negative:
+
+def hemis(x):
+    try:
+        if cti.CountryInfo(x).latlng()[0]>0:
+            return 'N'
+        else: return 'S'
+    except: return None
+
+
+#Function to calculate the percentage of fatal attacks over non-fatals (fatality):
+def fatality(lst):
+    l = list(lst)
+    return (l.count('Y')/len(l))*100
